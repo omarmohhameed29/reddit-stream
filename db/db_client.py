5fde8380adc4018af2514d292f2fd51fbda604db
omarmohhameed29 <omarmohhameed828@gmail.com>
2025-05-02 15:09:22 +0300

    update(reddit_posts table schema): add sentiment column

--- db/db_client.py
+++ db/db_client.py
@@ -27,18 +27,20 @@ def create_table_if_not_exists():
     if not table_exists:
         # Table doesn't exist, so create it
         create_table_query = """
-        CREATE TABLE reddit_posts (
-            id TEXT PRIMARY KEY,
-            title TEXT NOT NULL,
-            author TEXT,
-            score INTEGER,
-            num_comments INTEGER,
-            created_utc TIMESTAMP WITHOUT TIME ZONE,
-            subreddit TEXT,
-            url TEXT,
-            is_self BOOLEAN,
-            selftext TEXT
-        );
+            CREATE TABLE reddit_posts (
+                id TEXT PRIMARY KEY,
+                title TEXT NOT NULL,
+                author TEXT,
+                score INTEGER,
+                num_comments INTEGER,
+                created_utc TIMESTAMP WITHOUT TIME ZONE,
+                subreddit TEXT,
+                url TEXT,
+                is_self BOOLEAN,
+                selftext TEXT,
+                sentiment TEXT,
+                sentiment_score NUMERIC
+            );
         """
         cur.execute(create_table_query)
         conn.commit()
